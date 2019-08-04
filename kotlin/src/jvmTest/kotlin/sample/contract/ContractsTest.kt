97dd4199949ca1985b137c308475fab65c2da06c
SatoShun <shun.sato1@gmail.com>
2019-08-04 03:06:27 +0000

    contract

---
+++ kotlin/src/jvmTest/kotlin/sample/contract/ContractsTest.kt
@@ -0,0 +1,16 @@
+package sample.contract
+
+import org.junit.Test
+
+class ContractsTest {
+  @Test
+  fun a() {
+    val a: String? = "10"
+    require(a is String)
+    println(a.length)
+
+    synchronized(a) {
+    }
+    a.isNullOrEmpty()
+  }
+}
