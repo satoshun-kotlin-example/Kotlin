1d93cebad1750255388654f7086782ad7070f8c7
SatoShun <shun.sato1@gmail.com>
2019-08-06 00:39:39 +0000

    List

--- kotlin/src/jvmTest/kotlin/sample/list/List.kt
+++ kotlin/src/jvmTest/kotlin/sample/list/List.kt
@@ -1,10 +1,41 @@
 package sample.list
 
+import kotlinx.collections.immutable.persistentListOf
 import org.junit.Test
 
 class ListTest {
   @Test
   fun t() {
-    mutableListOf<Int>()
+    run {
+      val a = mutableListOf<String>("1", "2")
+      List2.l(a)
+      List2.l2(a)
+
+      val a2: List<Any> = a
+    }
+
+    run {
+      val a = listOf<String>("1", "2")
+      List2.l(a)
+      List2.l2(a)
+
+      val a2: List<Any> = a
+    }
+
+    run {
+      val a = persistentListOf<String>("1", "2")
+      List2.l(a)
+      List2.l2(a)
+    }
+  }
+
+  @Test
+  fun t2() {
+    run {
+      val a = mapOf("1" to 1, "2" to 2)
+      for (element in a) {
+        println(element.javaClass)
+      }
+    }
   }
 }
---
+++ kotlin/src/jvmTest/kotlin/sample/list/List2.java
@@ -0,0 +1,13 @@
+package sample.list;
+
+import java.util.Collection;
+import java.util.List;
+
+public class List2 {
+    public static void l(List<String> a) {
+        a.set(0, "");
+    }
+
+    public static void l2(Collection<String> a) {
+    }
+}
