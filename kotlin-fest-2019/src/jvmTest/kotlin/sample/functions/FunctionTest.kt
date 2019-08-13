461aba9ae0c84c21e74fa11adde50b75b54bc17a
SatoShun <shun.sato1@gmail.com>
2019-08-13 01:04:12 +0000

    hoge

--- kotlin-fest-2019/src/jvmTest/kotlin/sample/functions/FunctionTest.kt
+++ kotlin-fest-2019/src/jvmTest/kotlin/sample/functions/FunctionTest.kt
@@ -5,9 +5,33 @@ import org.junit.Test
 class FunctionTest {
   @Test
   fun test() {
-    hoge { }
+    hoge { a -> a.toString() }
+
+    val h3: (Int, String) -> String = ::hoge3
+    h3(10, "")
   }
 }
 
-private fun hoge(body: () -> Unit) {
+private fun hoge(body: (Int) -> String) {
+  body(10)
 }
+
+private fun hoge2(body: (Int, String) -> String) {
+  body(10, "hoge")
+}
+
+private fun hoge3(a1: Int, a2: String): String {
+  return a2
+}
+
+private fun hoge(
+  body: (
+    p1: Int, p2: Int, p3: Int, p4: Int, p5: Int, p6: Int, p7: Int, p8: Int, p9: Int, p10: Int,
+    p11: Int, p12: Int, p13: Int, p14: Int, p15: Int, p16: Int, p17: Int, p18: Int, p19: Int, p20: Int,
+    p21: Int, p22: Int,
+    p23: Int
+  ) -> Unit
+) {
+}
+
+fun <P1, P2, R> ((P1, P2) -> R).curried(): (P1) -> (P2) -> R = { p1: P1 -> { p2: P2 -> this(p1, p2) } }
