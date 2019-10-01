72080095ae850d178b5bad61400d70408c64f515
SatoShun <shun.sato1@gmail.com>
2019-10-01 00:14:53 +0000

    update

---
+++ kotlin/src/jvmTest/kotlin/sample/duration/DurationTest.kt
@@ -0,0 +1,11 @@
+package sample.duration
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.days
+
+class DurationTest {
+  @ExperimentalTime
+  fun test() {
+    10.days
+  }
+}
--- kotlin/src/jvmTest/kotlin/sample/enu/EnumTest.kt
+++
@@ -1,4 +0,0 @@
-package sample.enu
-
-class EnumTest {
-}
